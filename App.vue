94b93c6d9bd619853425d0c9a393de2d495a3863
raqib907 <106022396+raqib907@users.noreply.github.com>
2023-08-21 23:36:57 +0600

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import ChildComponent from "./components/ChildComponent.vue";
+import { ref } from 'vue';
+
+const parentMessage = ref("Hello from parent!");
+const childMessage = ref("");
+
+const handleChildEvent = (message) => {
+  childMessage.value = message;
+};
+</script>
+<template>
+  <div class="bg-gray-200 min-h-screen flex items-center justify-center">
+    <ChildComponent :message="parentMessage" @child-event="handleChildEvent" />
+    <div class="mt-4 text-center">
+      <p class="text-xl">Message from child: {{ childMessage }}</p>
+    </div>
+  </div>
+</template>
+
+
+
+<style scoped>
+
+</style>
---
+++ components/ChildComponent.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { defineProps, defineEmits } from 'vue';
+
+const props = defineProps({
+  message: String,
+});
+
+const { emit } = defineEmits();
+
+const sendMessageToParent = () => {
+  emit('child-event', 'Hello from child!');
+};
+</script>
+<template>
+  <div class="bg-white p-4 rounded shadow">
+    <p class="text-lg">{{ message }}</p>
+    <button @click="sendMessageToParent" class="mt-2 bg-blue-500 hover:bg-blue-600 text-white px-3 py-2 rounded">
+      Send Message to Parent
+    </button>
+  </div>
+</template>
+
+
+
+<style scoped>
+
+</style>
